bc7924c40a4433bce84c90059cd4de7b40ee272d
MLankaoui <marouanelankaoui66@gmail.com>
2024-09-01 22:43:56 +0100

    first commit

--- DANDO/settings.py
+++ DANDO/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+$^jk)49ricnq_!+i^k6@r44ss=tjb!#_l!xgyfl@dmprle%b7'
+SECRET_KEY = 'django-insecure-xz2uexr7wvab1_j^&)izc(&xpr10&!*gfx7070hc&)hf!(w^0v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
