757e52e966431627d6daa3e3b2b8c39886901022
MLankaoui <marouanelankaoui66@gmail.com>
2024-09-02 08:52:36 +0100

    register todos in the admin page.

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from todo.models import Todos
 
-# Register your models here.
+
+@admin.register(Todos)
+class TodosAdmin(admin.ModelAdmin):
+    list_display = ["id", "content"]
