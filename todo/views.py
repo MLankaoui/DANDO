e61d022340ccf3701842c5b0a836d691e0697b17
MLankaoui <marouanelankaoui66@gmail.com>
2024-09-02 09:08:08 +0100

    implement adding new tasks.

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from todo.models import Todos
 
 
-# home page
 def home(request):
-    return render(request, 'index.html')
+    if request.method == "POST":
+        content = request.POST.get('content')
+        if content != None:
+            Todos.objects.create(content=content)
+            return redirect('home')
+
+    todos = Todos.objects.all()
+    return render(request, 'index.html', {'todos': todos})
