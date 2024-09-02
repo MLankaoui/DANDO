3f2b8b69efc00abe347d1dcae8f0ada1c32ce13e
MLankaoui <marouanelankaoui66@gmail.com>
2024-09-02 08:11:28 +0100

    configure mysql in django and and migrate.

--- DANDO/settings.py
+++ DANDO/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'DANDO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DANDO',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
