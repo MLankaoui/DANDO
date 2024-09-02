371d636bda50d7e67546c520cd98343efdc908f8
MLankaoui <marouanelankaoui66@gmail.com>
2024-09-02 08:37:07 +0100

    create todos model.

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+# todos model
+class Todos(models.Model):
+    content = models.CharField(max_length=100, null=False)
+    class Meta:
+        managed = True
+        db_table = 'todos'
+
+
+    def __str__(self) -> str:
+        return self.content
